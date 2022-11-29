aa701f2006712b8d6adc1f0a470ab20786e8c888
Emdadul Islam <bekon1700@gmail.com>
2022-11-29 20:35:58 +0600

    stripe payment route added

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config({
+    path: './config.env'
+})
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
 
 const productRouter = require('./routes/productRoute')
 const authRouter = require('./routes/authRoute')
@@ -16,6 +24,22 @@ app.get('/', (req, res) => {
     res.status(200).send('API IS RUNNING....')
 })
 
+
+app.post('/create-payment-intent', async (req, res) => {
+    const totalBill = req.body.totalBill * 100
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: parseInt(totalBill),
+        currency: "usd",
+        payment_method_types: [
+            'card'
+        ]
+    });
+
+    res.status(200).json({
+        status: 'success',
+        client_secret: paymentIntent.client_secret
+    })
+})
 
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/users', authRouter);
